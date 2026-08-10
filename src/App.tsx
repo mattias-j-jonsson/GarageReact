63ea16897c6de5aac845907d7bc9dc03c04a8707
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-08-10 15:47:55 +0200

    add basic structure in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 function App() {
-  
+  return (
+    // Rubrik och bilräknare
+    <div>
+      <header>
+        <h1>Minigaraget</h1>
+        <p>Antal bilar i garaget: </p>
+      </header>
+      {/* Parkeringsformulär */}
+      <div>
+        <h2>Parkera nytt fordon</h2>
+        <form action="">
+          <label htmlFor="regnr">Registreringsnummer</label>
+          <input type="text" required placeholder="t ex ABC123 eller DEF45G"/>
+          <label htmlFor="märke">Märke</label>
+          <input type="text" required placeholder="t ex Volvo"/>
+          <button>Parkera bil</button>
+        </form>
+      </div>
+      {/* Parkerade fordon */}
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
