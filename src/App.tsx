cb50a28f5b5956a8cbe465bea5db659351f589d0
Mattias Jönsson <mattias.j.jonsson@hotmail.com>
2026-08-10 16:40:36 +0200

    some basic styling and header as its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 // import { useState } from 'react'
 
+import Header from "./Header";
+
 function App() {
   return (
     // Rubrik och bilräknare
-    <div>
-      <header>
-        <h1>Minigaraget</h1>
-        <p>Antal bilar i garaget: </p>
-      </header>
+    <div className="mx-auto w=2/3 bg-slate-950">
+      <Header />
       {/* Parkeringsformulär */}
       <div>
-        <h2>Parkera nytt fordon</h2>
+        <h2 className="text-emerald-400">Parkera nytt fordon</h2>
         <form action="">
-          <label htmlFor="regnr">Registreringsnummer</label>
+          <label htmlFor="regnr" className="text-slate-400">Registreringsnummer</label>
           <input type="text" required placeholder="t ex ABC123 eller DEF45G"/>
-          <label htmlFor="märke">Märke</label>
+          <label htmlFor="märke" className="text-slate-400">Märke</label>
           <input type="text" required placeholder="t ex Volvo"/>
-          <button>Parkera bil</button>
+          <button className="bg-emerald-600">Parkera bil</button>
         </form>
       </div>
       {/* Parkerade fordon */}
-      
+      <div>
+        <h2 className="text-emerald-400">Parkerade fordon</h2>
+      </div>
     </div>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+        <h1 className="text-emerald-400 font-bold">Minigaraget</h1>
+        <p className="text-slate-400">Antal bilar i garaget: </p>
+      </header>
+    );
+}
+
+export default Header;
